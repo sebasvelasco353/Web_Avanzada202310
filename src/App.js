fb3b66fe362e178d76db705bc8521e1b66a765e8
Diego H <d13g01hl@gmail.com>
2023-02-07 19:21:17 -0500

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/Footer.js'
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+      <footer className="Footer">
+        <p>this is a footer</p>
+        <p>this is a footer</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
