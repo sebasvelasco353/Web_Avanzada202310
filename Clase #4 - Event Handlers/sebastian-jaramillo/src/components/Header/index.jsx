ab8f0077b51728411f246edd24d947ca7f5d403d
Sebastian Jaramillo <sebastian.jaramillo2@u.icesi.edu.co>
2023-02-21 19:55:24 -0500

    Actividad clase 4

---
+++ Clase #4 - Event Handlers/sebastian-jaramillo/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import './Header.css'
+
+function Header() {
+    
+    return (
+        <div className="header">
+            <p>Students</p>
+        </div>
+    )
+}
+
+export default Header
