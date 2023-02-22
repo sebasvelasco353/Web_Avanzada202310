881f487a6943282b582bab71e2b89e8861a7d59d
Juan Fernando Angulo <juanferas25@hotmail.com>
2023-02-21 19:53:36 -0500

    Class 4 exercise Advance

---
+++ Clase #4 - Event Handlers/juan-angulo/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import React, { useState } from 'react';
+import Content from './components/Content';
+
+export const StudentsContext = React.createContext();
+
+function App() {
+
+  const [students, setStudents] = React.useState([
+    {
+      name: 'Juan',
+      age: 20,
+      career: 'Sistemas',
+      semester: 8
+    },
+    {
+      name: 'Pedro',
+      age: 18,
+      career: 'Sistemas',
+      semester: 6
+    }
+  ]);
+
+  const modifySemester = (idx, action) => {
+    const newArr = students.map(student => {
+      if (students.indexOf(student) === idx) {
+        if (action === "increase") {
+          student.semester += 1;
+        } else if (action === "decrease") {
+          student.semester -= 1;
+        }
+      }
+      return student;
+    });
+    setStudents(newArr)
+  }
+
+  const removeStudent = (idx) => {
+    setStudents(students.splice(idx + 1, 1));
+  }
+
+  return (
+    <div className="App">
+      <StudentsContext.Provider value={students}>
+        <Content modifySemester={modifySemester} removeStudent={removeStudent} />
+      </StudentsContext.Provider>
+    </div>
+  );
+}
+
+export default App;
