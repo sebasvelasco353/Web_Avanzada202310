a604a67955cb8d7dce9868306631ccd939b85415
Juan Fernando Angulo Salvador <1005877854@u.icesi.edu.co>
2023-02-22 19:58:17 -0500

    EditStudent advance

--- Clase #4 - Event Handlers/juan-angulo/src/components/EditStudent/index.jsx
+++ Clase #4 - Event Handlers/juan-angulo/src/components/EditStudent/index.jsx
@@ -1,20 +1,31 @@
 import React, { useContext } from 'react';
 import { StudentsContext } from '../../App';
-import './Content.css';
+import './EditStudent.css';
 
 function EditStudent({ idx }) {
     const students = useContext(StudentsContext);
     return (
-        <div className="Container">
+        <div className="Container" style={{ alignItems: 'center', justifyContent: 'center' }}>
             <h2>Edit Student</h2>
-            <p> Name: </p>
-            <input type="text" value={students[idx].name} />
-            <p> Age: </p>
-            <input type="text" value={students[idx].age} />
-            <p> Career: </p>
-            <input type="text" value={students[idx].career} />
-            <p> Semester: </p>
-            <input type="text" value={students[idx].semester} />
+            <div style={{ width: 'fit-content', margin: 'auto' }}>
+                <div className="field">
+                    <p> Name: </p>
+                    <input type="text" value={students.at(idx).name} />
+                </div>
+                <div className="field">
+                    <p> Age: </p>
+                    <input type="text" value={students.at(idx).age} />
+                </div>
+                <div className="field">
+                    <p> Career: </p>
+                    <input type="text" value={students.at(idx).career} />
+                </div>
+                <div className="field">
+                    <p> Semester: </p>
+                    <input type="text" value={students.at(idx).semester} />
+                </div>
+                <button>Save</button>
+            </div>
         </div>
     )
 }
