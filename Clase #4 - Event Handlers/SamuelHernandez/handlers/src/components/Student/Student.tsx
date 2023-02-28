5d0d5fa5a6acb70359bc90fe168ba57dfbc51fe3
Samuel Hernandez <1006011433@u.icesi.edu.co>
2023-02-28 01:32:16 -0500

    feat: added content

--- Clase #4 - Event Handlers/SamuelHernandez/handlers/src/components/Student/Student.tsx
+++ Clase #4 - Event Handlers/SamuelHernandez/handlers/src/components/Student/Student.tsx
@@ -1,9 +1,49 @@
 import {StudentType} from "./StudentType";
+import "./Student.css";
+import {Plus, Dash, Trash} from 'react-bootstrap-icons';
+import React from "react";
 
-export const Student = (props : StudentType) => {
+export const Student = (props: StudentType) => {
+    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        alert("Student Clicked!")
+    };
+
+    const increaseSemester = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        alert("Increment!")
+    };
+
+    const decreaseSemester = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        alert("Decrement!")
+    };
+
+    const removeStudent = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        alert("Bye!!")
+    };
+
     return (
-       <section className={"student"}>
-
-       </section>
+        <article className={"student"} onClick={handleClick}>
+            <p>{props.id}</p>
+            <p>{props.name}</p>
+            <p> {props.age}</p>
+            <p>{props.career}</p>
+            <div className={"student__semester"}>
+                <button onClick={increaseSemester}
+                        className={"student__button student__button--add"}><Plus size={26}/>
+                </button>
+                <p>{props.semester}</p>
+                <button onClick={decreaseSemester}
+                        className={"student__button student__button--remove"}><Dash size={26}/>
+                </button>
+            </div>
+            <button className={"student__button student__button--delete"} onClick={removeStudent}><Trash size={26}/>
+            </button>
+        </article>
     );
 }
--- Clase #4 - Event Handlers/SamuelHernandez/handlers/src/components/Table/Table.tsx
+++ Clase #4 - Event Handlers/SamuelHernandez/handlers/src/components/Table/Table.tsx
@@ -1,14 +1,35 @@
+import StudentContext from "../../context/Student/StudentContext";
+import {useContext} from "react";
+import {Student} from "../Student/Student";
+import {StudentProvider} from "../../context/Student/StudentProvider";
+import "./Table.css";
+
 interface IProps {
-    name : string,
-    age : number,
-    career : string,
-    semester : number,
+
 }
 
-export const Table = (props : IProps) => {
-    return (
-        <section className={"student"}>
+export const Table = (props: IProps) => {
 
-        </section>
+    const {students, addStudent, removeStudent, updateStudent} = useContext(StudentContext);
+    console.log(students);
+    return (
+        <StudentProvider>
+            <section className={"table"}>
+                <section className={"table__headers"}>
+                    <h3>Code</h3>
+                    <h3>Name</h3>
+                    <h3>Age</h3>
+                    <h3>Career</h3>
+                    <h3>Semester</h3>
+                    <h3>-</h3>
+                </section>
+                <section className={"table__body"}>
+                    {students.map((student) => {
+                        const {id, name, age, career, semester} = student;
+                        return <Student key={id} id={id} name={name} age={age} career={career} semester={semester}/>
+                    })}
+                </section>
+            </section>
+        </StudentProvider>
     );
 }
