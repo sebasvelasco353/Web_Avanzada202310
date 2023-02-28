db8461763cdce7bac201f5ffa41a686a9106375c
Samuel Hernandez <1006011433@u.icesi.edu.co>
2023-02-28 11:52:36 -0500

    feat: connected student context to app

--- Clase #4 - Event Handlers/SamuelHernandez/handlers/src/components/Table/Table.tsx
+++ Clase #4 - Event Handlers/SamuelHernandez/handlers/src/components/Table/Table.tsx
@@ -10,26 +10,25 @@ interface IProps {
 
 export const Table = (props: IProps) => {
 
-    const {students, addStudent, removeStudent, updateStudent} = useContext(StudentContext);
-    console.log(students);
+    const {students} = useContext(StudentContext);
+
     return (
-        <StudentProvider>
-            <section className={"table"}>
-                <section className={"table__headers"}>
-                    <h3>Code</h3>
-                    <h3>Name</h3>
-                    <h3>Age</h3>
-                    <h3>Career</h3>
-                    <h3>Semester</h3>
-                    <h3>-</h3>
-                </section>
-                <section className={"table__body"}>
-                    {students.map((student) => {
-                        const {id, name, age, career, semester} = student;
-                        return <Student key={id} id={id} name={name} age={age} career={career} semester={semester}/>
-                    })}
-                </section>
+
+        <section className={"table"}>
+            <section className={"table__headers"}>
+                <h3>Code</h3>
+                <h3>Name</h3>
+                <h3>Age</h3>
+                <h3>Career</h3>
+                <h3>Semester</h3>
+                <h3>-</h3>
             </section>
-        </StudentProvider>
+            <section className={"table__body"}>
+                {students.map((student) => {
+                    const {id, name, age, career, semester} = student;
+                    return <Student key={id} id={id} name={name} age={age} career={career} semester={semester}/>
+                })}
+            </section>
+        </section>
     );
 }
