30b7783045a56d72b4871033c3aef4be5de221b8
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2023-02-28 23:13:03 -0500

    WIP - added comments explaining the studentsReducer file

--- ejerciciosResueltos/clase4/js_context/src/context/studentsReducer.js
+++ ejerciciosResueltos/clase4/js_context/src/context/studentsReducer.js
@@ -1,8 +1,12 @@
-import * as React from 'react'
+import * as React from 'react';
 import { students } from '../assets/students';
 
-const StudentsContext = React.createContext()
+const StudentsContext = React.createContext(); // Creamos el contexto que usaremos para pasar el estado.
 
+/*
+** Mi reducer recibe el estado actual y una accion, y dependiendo del tipo de accion va **
+** a modificar el estado, despues va a retornar ese nuevo estado generando un re render  **
+*/
 function StudentsReducer(state, action) {
   switch (action.type) {
     case 'increment': {
@@ -39,12 +43,21 @@ function StudentsReducer(state, action) {
   }
 }
 
-function StudentsProvider({children}) {
+/*
+** El provider es un componente que se va a encargar de entregar el contexto o estado **
+** y el metodo dispatch a los componentes hijos, en este caso el componente Table que **
+** usamos en App.JS                                                                   **
+*/
+function StudentsProvider({ children }) {
   const [state, dispatch] = React.useReducer(StudentsReducer, { students })
   const value = {state, dispatch}
   return <StudentsContext.Provider value={value}>{children}</StudentsContext.Provider>
 }
 
+/*
+** useStudents es una funcion que nos devuelve el context de la aplicacion y lo vamos **
+** a usar siempre que necesitemos acceder al state o cuando vamos a usar dispatch     **
+*/
 function useStudents() {
   const context = React.useContext(StudentsContext)
   if (context === undefined) {
