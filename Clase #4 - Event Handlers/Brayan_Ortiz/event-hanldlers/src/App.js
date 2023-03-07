e822c937105c4eea181375c1cb3080c8bb2925e7
BrayanOB2003 <brayanortizbastidas82@gmail.com>
2023-03-07 17:29:10 -0500

    avance

---
+++ Clase #4 - Event Handlers/Brayan_Ortiz/event-hanldlers/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Table from './components/Table/Table'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <Table students={[]}/>
+    </div>
+  );
+}
+
+export default App;
