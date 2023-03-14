0f9f4d1822f39e8fcdf948355fc1c4a5403f2a88
brianR18 <briomero76@gmail.com>
2023-03-14 18:49:45 -0500

    Firts commit: App template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter,
+    Router,
+    Routes
+  } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+import Products from "./components/products-view";
+import Cart from "./components/cart";
+
+function App(){
+    return(
+        <HashRouter>
+            <div>
+                <ul className="header">
+                    <li><NavLink to="/products">Ver productos</NavLink></li>
+                    <li><NavLink to="/cart">Carrito</NavLink></li>
+                    <li><NavLink to="/">Usuario</NavLink></li>
+                    <li><NavLink to="/">Logout</NavLink></li>
+                </ul>
+            </div>
+            <div className="content">
+                <Routes>
+                    <Route path="/products" element={<Products/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                </Routes>
+            </div>
+        </HashRouter>
     );
-  }
 }
 
 export default App;
---
+++ src/components/products-view.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Products(){
+    return(
+        <div><h1>Hola</h1></div>
+    );
+}
+
+export default Products;
