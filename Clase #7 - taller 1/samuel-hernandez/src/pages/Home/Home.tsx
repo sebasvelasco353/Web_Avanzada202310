50000d1739f367d7783d7c920edc75d64486b239
Samuel Hernandez <1006011433@u.icesi.edu.co>
2023-03-26 19:14:28 -0500

    feat (Home): added basic home and item card layout

--- Clase #7 - taller 1/samuel-hernandez/src/pages/Home/Home.tsx
+++ Clase #7 - taller 1/samuel-hernandez/src/pages/Home/Home.tsx
@@ -1,10 +1,23 @@
+import "./Home.css";
+import {AvailableItems} from "../../data/AvailableItems";
+import {ItemCard} from "../../components/ItemCard/ItemCard";
 interface IProps {
 
 }
 
 export const Home = (props: IProps) => {
-    return (
-        <main className={"home"}>
-        </main>
-    );
+
+
+    return <main className={"home"}>
+        <header className={"home__header"}>
+
+        </header>
+        <section className={"home__wrapper"}>
+            <section className={"home__content"}>
+                {AvailableItems.map((item, index) => {
+                    return <ItemCard key={`itemCard${index}`} item={item} />;
+                })}
+            </section>
+        </section>
+    </main>;
 }
