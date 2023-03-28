e87153b0b9771f22c40c483b182ee52fb8498fa0
Samuel Hernandez <1006011433@u.icesi.edu.co>
2023-03-28 18:57:14 -0500

    fix (Modal): fixed item modal styling and behavior

--- Clase #7 - taller 1/samuel-hernandez/src/components/ItemModal/ItemModal.tsx
+++ Clase #7 - taller 1/samuel-hernandez/src/components/ItemModal/ItemModal.tsx
@@ -1,14 +1,10 @@
 import "./ItemModal.css";
 import {Plus, Dash} from "react-bootstrap-icons";
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useState} from "react";
 import ModalContext from "../../context/Modal/ModalContext";
 import CartContext from "../../context/Cart/CartContext";
 
-interface IProps {
-
-}
-
-export const ItemModal = (props: IProps) => {
+export const ItemModal = () => {
     const {isOpen, item, initialQuantity, close} = useContext(ModalContext);
     const {addItem, updateItem, removeItem, findItemById} = useContext(CartContext);
     const itemInCart = findItemById(item.id);
@@ -64,7 +60,7 @@ export const ItemModal = (props: IProps) => {
         dismiss();
     };
 
-    const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const handleCancel = (e: React.MouseEvent<HTMLButtonElement | HTMLSpanElement>) => {
         e.preventDefault();
         setQuantity(0);
         dismiss();
@@ -73,38 +69,42 @@ export const ItemModal = (props: IProps) => {
     const inStock = item.stock > 0;
 
     return (
-        <aside className={`item-modal ${isOpen ? "shown" : "hidden"}`}>
-            <section>
-                <h2>{item.name}</h2>
-                <h4 className={inStock ? "available" : "unavailable"}>{inStock ? `${item.stock} in stock` : "Out of stock"}</h4>
-                <p className={"item-modal__description"}>{item.description}</p>
-                <h3>$ {item.price}</h3>
-                <p className={"item-modal__shipping"}>+shipping</p>
-            </section>
-            <section>
-                <section className={"item-modal__add"}>
-                    <section className={"item-modal__add__quantity"}>
-                        <button className={"item-modal__add__quantity__add"} disabled={addDisable || !inStock}
-                                onClick={add}>
-                            <Plus size={32}/>
-                        </button>
-                        <input disabled={!inStock} type={"number"} max={100} min={0}
-                               className={"item-modal__add__quantity__number"}
-                               onChange={handleChange} value={quantity} onBlur={handleBlur}/>
-                        <button className={"item-modal__add__quantity__remove"} disabled={removeDisable || !inStock}
-                                onClick={remove}>
-                            <Dash size={32}/>
-                        </button>
-                    </section>
-                    <section className={"item-modal__add__button-bar"}>
-                        <button disabled={!inStock} onClick={handleConfirm}
-                                className={"item-modal__add__button-bar__confirm"}>Confirm
-                        </button>
-                        <button onClick={handleCancel} className={"item-modal__add__button-bar__cancel"}>Cancel</button>
+        <>
+            <span onClick={handleCancel} className={`item-modal__overlay ${isOpen ? "modal-shown" : "modal-hidden"}`}/>
+            <aside className={`item-modal ${isOpen ? "modal-shown" : "modal-hidden"}`}>
+                <section>
+                    <h2>{item.name}</h2>
+                    <h4 className={inStock ? "available" : "unavailable"}>{inStock ? `${item.stock} in stock` : "Out of stock"}</h4>
+                    <p className={"item-modal__description"}>{item.description}</p>
+                    <h3>$ {item.price}</h3>
+                    <p className={"item-modal__shipping"}>+shipping</p>
+                </section>
+                <section>
+                    <section className={"item-modal__add"}>
+                        <section className={"item-modal__add__quantity"}>
+                            <button className={"item-modal__add__quantity__add"} disabled={addDisable || !inStock}
+                                    onClick={add}>
+                                <Plus size={32}/>
+                            </button>
+                            <input disabled={!inStock} type={"number"} max={100} min={0}
+                                   className={"item-modal__add__quantity__number"}
+                                   onChange={handleChange} value={quantity} onBlur={handleBlur}/>
+                            <button className={"item-modal__add__quantity__remove"} disabled={removeDisable || !inStock}
+                                    onClick={remove}>
+                                <Dash size={32}/>
+                            </button>
+                        </section>
+                        <section className={"item-modal__add__button-bar"}>
+                            <button disabled={!inStock} onClick={handleConfirm}
+                                    className={"item-modal__add__button-bar__confirm"}>Confirm
+                            </button>
+                            <button onClick={handleCancel} className={"item-modal__add__button-bar__cancel"}>Cancel
+                            </button>
+                        </section>
                     </section>
                 </section>
-            </section>
 
-        </aside>
+            </aside>
+        </>
     );
 }
