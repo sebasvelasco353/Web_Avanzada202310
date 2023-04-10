b1081677c8ae72c56e57fac9c276ea0c3b278b6d
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2023-04-09 19:22:22 -0500

    added class about storybook

--- Clase #9 - Express example/routes/index.js
+++ Clase #9 - Express example/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/claseWeb', function(req, res, next) {
+  // llamamos a la bd y procesamos lo que me pidieron
+  res.status(200).send({
+    cosa: 'q',
+    objeto: {
+      a: 'b'
+    }
+  })
+})
+
 module.exports = router;
