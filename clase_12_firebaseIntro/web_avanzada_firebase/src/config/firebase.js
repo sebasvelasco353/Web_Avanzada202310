09f5e693162a27ebf2c2cfbd8815697ecb6d924b
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2023-04-25 14:13:20 -0500

    added firebse intro call

---
+++ clase_12_firebaseIntro/web_avanzada_firebase/src/config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7SzLRvfAKNkzvKrFQqZ6jwxExhRNeeWQ",
+  authDomain: "web-avanzada-24977.firebaseapp.com",
+  projectId: "web-avanzada-24977",
+  storageBucket: "web-avanzada-24977.appspot.com",
+  messagingSenderId: "276915523087",
+  appId: "1:276915523087:web:479f6c18a60e736ee31d7a",
+  measurementId: "G-Z9PHQ1W9VQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app);
+export const db = getFirestore(app);
