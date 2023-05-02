6359744d1e150d17b3dbe44fb1f264ac602d3bec
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2023-05-02 13:27:16 -0500

    clase 13

---
+++ clase_13_firebase2/web_avanzada_firebase/src/components/ListItem/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+
+export default function ListItem({item}) {
+  const [newScore, setNewScore] = useState(0);
+  const handleDelete = async () => {
+    const movieDoc = doc(db, "movies", item.id);
+    console.log('delete element with id: ', item.id);
+    await deleteDoc(movieDoc);
+  }
+  const handleEditScore = async () => {
+    const movieDoc = doc(db, "movies", item.id);
+    console.log('delete element with id: ', item.id);
+    await updateDoc(movieDoc, {score: newScore});
+  }
+
+  return (
+    <div>
+        <h3>{item.title} - {item.release_date}</h3>
+        <p>{item.director}</p>
+        <p>score: {item.score}</p>
+        <button onClick={handleDelete}>Delete</button>
+        <input
+          placeholder='score'
+          type="number"
+          onChange={(e) => setNewScore(e.target.value)}
+        />
+        <button onClick={handleEditScore}>update score</button>
+    </div>
+  )
+}
