99c126cc2b5cee5b04f041f50c07b7959cd6d161
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2023-05-09 18:15:26 -0500

    added user profile data fetch

--- clase_14_firebase3/web_avanzada_firebase/src/components/SignIn/index.jsx
+++ clase_14_firebase3/web_avanzada_firebase/src/components/SignIn/index.jsx
@@ -1,23 +1,32 @@
-import { firebaseAuth, db } from "../../config/firebase";
+import { firebaseAuth, db, storage } from "../../config/firebase";
+import { v4 } from "uuid";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import './signin.css'
 import { addDoc, collection } from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 function SignIn() {
   const usersCollectionRef = collection(db, "users"); // h
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [file, setFile] = useState(null);
 
   const handleSignIn = () => {
+    const filesFolderReference = ref(storage, `userImages/${v4()+file.name}`); // save in the random folder
     try {
-        createUserWithEmailAndPassword(firebaseAuth, email, password).then((res) => {
-          addDoc(usersCollectionRef, { edad: 12, test: true, uid: res.user.uid});
-        })
+        uploadBytes(filesFolderReference, file).then(res => {
+          return getDownloadURL(res.ref);
+        }).then(url => {
+          createUserWithEmailAndPassword(firebaseAuth, email, password).then((res) => {
+            addDoc(usersCollectionRef, { edad: 12, test: true, uid: res.user.uid, profileImage: url});
+          });
+        });
     } catch (error) {
         console.error(error);
     }
   }
+
   return (
     <div className="SignIn">
         <h3>Dont have an account?</h3>
@@ -30,6 +39,9 @@ function SignIn() {
           type='password'
           onChange={(e) => setPassword(e.target.value)}
         />
+        <div>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])}/>
+        </div>
         <button onClick={handleSignIn}>Sign In</button>
     </div>
   )
