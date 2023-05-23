9e986329e5b1446a6770dac161d0ff0dd9b0069d
zacwastaken <seamzacdisc1@gmail.com>
2023-05-23 16:03:44 -0500

    feat: connected items to firestore

--- Taller2/samuel-hernandez/src/pages/Home/Home.tsx
+++ Taller2/samuel-hernandez/src/pages/Home/Home.tsx
@@ -3,8 +3,30 @@ import {ItemCard} from "../../components/ItemCard/ItemCard";
 import {ItemModal} from "../../components/ItemModal/ItemModal";
 import {ModalProvider} from "../../context/Modal/ModalProvider";
 import {Helmet} from "react-helmet";
+import {collection, doc, getDoc, getDocs, query} from "firebase/firestore";
+import {db} from "../../config/firebase";
+import {Item, User} from "../../interfaces/interfaces";
+import {useEffect, useState} from "react";
 
 export const Home = () => {
+
+    const [items, setItems] = useState([] as Item[]);
+
+    const fetchItems = async () => {
+        const itemsQuery = query(collection(db, "items"));
+
+        const querySnapshot = await getDocs(itemsQuery);
+        querySnapshot.forEach((doc) => {
+            const itemizedDoc = { id: doc.id, ...doc.data()} as Item;
+            setItems(items.concat(itemizedDoc));
+        });
+
+    }
+
+    useEffect(() => {
+        fetchItems();
+    }, [])
+
     return (
         <ModalProvider>
             <Helmet>
@@ -16,7 +38,11 @@ export const Home = () => {
                 </header>
                 <section className={"home__wrapper"}>
                     <section className={"home__content"}>
-
+                        {items.map((item) => {
+                            return (
+                                <ItemCard key={`item${item.id}`} item={item}/>
+                            );
+                        })}
                     </section>
                 </section>
                 <ItemModal/>
