130a98b83c11150fc916a52c6eac2b67b8b47f3a
BrayanOB2003 <brayanortizbastidas82@gmail.com>
2023-06-05 02:03:50 -0500

    Modify close modals method and request bug

---
+++ Taller2/brayan-ortiz/src/components/login/Login.jsx
@@ -0,0 +1,49 @@
+import './Login.css'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { auth } from '../../config/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch()
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+    
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+            dispatch({type: 'Login'})
+            alert("Usuario autenticado.") 
+        } catch (error) {
+            alert("El usuario no se encuentra registrado.")
+        }
+    };
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+        <div className='form_container'>
+            <label>
+                Email:
+                <input type="email" id='email' value={email} onChange={handleEmailChange} required/>
+            </label>
+            <label>
+                Password:
+                <input type="password" id='password' value={password} onChange={handlePasswordChange} required/>
+            </label>
+            <button type="submit">LogIn</button>
+        </div>  
+    </form>
+    
+  );
+};
+
+export default Login;
---
+++ Taller2/brayan-ortiz/src/components/product/Product.jsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+import Modal from '../modal/Modal'
+import './Product.css'
+import { useSelector,useDispatch } from 'react-redux'
+import { db } from '../../config/firebase';
+import { deleteDoc, doc } from '@firebase/firestore';
+import UpdateModal from '../updateModal/UpdateModal';
+
+function Product (props){
+    
+    const [showEditModal, setShowEdit] = useState(false)
+    const showModal = useSelector(state => state.showModal);
+    const dispatch = useDispatch();
+
+    const clickProductCard =()=>{
+
+        if(showModal){
+            dispatch({type: 'HideModal'});
+        } else {
+            dispatch({type: 'SetProduct', payload: props})
+            dispatch({type: 'ShowModal'})
+        }
+    }
+
+    const displayModal = ()=> {
+        if(showModal){
+            return <Modal name={props.name} price={props.price} description={props.description} showModal={showModal}/>
+        }
+    }
+
+    const handleDelete=async()=>{
+        if(window.confirm('¿Estás segur@ que quieres eliminar este producto?')){
+            const productDoc = doc(db, 'product', props.id)
+            await deleteDoc(productDoc)
+        }
+    }
+
+    const handleEdit=()=>{
+        if(showEditModal){
+            setShowEdit(false)
+        } else {
+            setShowEdit(true)
+        }
+    }
+
+    return(
+        <div className='productcard'>
+            {props.active && <div className='productcard_bar'>
+                <button className='productcard_bar__delete_button' onClick={handleDelete}></button>
+                <button className='productcard_bar__edit_button' onClick={handleEdit}></button>
+            </div>}
+            <div className='productcard_container__info' onClick={clickProductCard}>
+                <h1>{props.name}</h1>
+                <h2>${props.price}</h2>
+                <p>{props.description}</p>
+            </div>
+            <UpdateModal product={props} isVisible ={showEditModal} setShowEdit={setShowEdit}/>
+            {displayModal()}
+        </div>       
+    )
+}
+
+export default Product;
